b503a0fb71d5d9849da9f4d822ee48ab9fe4974c
Flavio Jr <aaa@MacBook-Air-de-Flavio-Junior.local>
2026-05-18 20:08:29 -0300

    feat: apply GQuick white-label baseline

--- app/javascript/dashboard/App.vue
+++ app/javascript/dashboard/App.vue
@@ -2,9 +2,7 @@
 import { mapGetters } from 'vuex';
 import LoadingState from './components/widgets/LoadingState.vue';
 import NetworkNotification from './components/NetworkNotification.vue';
-import UpdateBanner from './components/app/UpdateBanner.vue';
 import StatusBanner from './components/app/StatusBanner.vue';
-import PaymentPendingBanner from './components/app/PaymentPendingBanner.vue';
 import PendingEmailVerificationBanner from './components/app/PendingEmailVerificationBanner.vue';
 import vueActionCable from './helper/actionCable';
 import { useRouter } from 'vue-router';
@@ -27,9 +25,7 @@ export default {
   components: {
     LoadingState,
     NetworkNotification,
-    UpdateBanner,
     StatusBanner,
-    PaymentPendingBanner,
     WootSnackbarBox,
     PendingEmailVerificationBanner,
   },
@@ -138,11 +134,9 @@ export default {
     class="flex flex-col w-full h-screen min-h-0 bg-n-background"
     :dir="isRTL ? 'rtl' : 'ltr'"
   >
-    <UpdateBanner :latest-chatwoot-version="latestChatwootVersion" />
     <StatusBanner />
     <template v-if="currentAccountId">
       <PendingEmailVerificationBanner v-if="hideOnOnboardingView" />
-      <PaymentPendingBanner v-if="hideOnOnboardingView" />
     </template>
     <router-view v-slot="{ Component }">
       <transition name="fade" mode="out-in">
